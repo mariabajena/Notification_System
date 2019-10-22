7708b7373a52d57737c11a42f94e77f843554845
Ania <azawadzka@vp.pl>
2019-10-22 14:12:51 +0200

    Initial commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public class Account {
+    List<Rule> userRules;
+}
---
+++ src/main/java/Rule.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public class Rule {
+    List<Condition> conditions;
+    List<Action> actions;
+}
